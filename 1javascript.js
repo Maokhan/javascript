96679f08613251d8582921f6b6fe94b7ef6d2146
Maokhan <ftal6779@gmail.com>
2024-05-12 06:36:23 +0000

    ssss

--- 1javascript.js
+++
@@ -1,9 +0,0 @@
-console.log("hello")//hello
-console.table(["ARBAZ","KHAN"])
-/*
-┌─────────┬─────────┐
-│ (index) │ Values  │
-├─────────┼─────────┤
-│ 0       │ 'ARBAZ' │
-│ 1       │ 'KHAN'  │
-└─────────┴─────────┘ */
---
+++ javascript/2string.js
@@ -0,0 +1,10 @@
+//  String
+
+const hello= "ajsajddsbcjs"
+
+ console.log( hello[5])
+ console.log( hello.__proto__)
+ console.log( hello.length)
+ console.log( hello.toUpperCase)
+ console.log( hello.charAt(3))
+ console.log( hello.indexOf("s"))
