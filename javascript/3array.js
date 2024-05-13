4af6ef23cd1f667dcb0b43d39829aaddf9993d45
Maokhan <ftal6779@gmail.com>
2024-05-13 06:30:18 +0000

    array complete

--- javascript/3array.js
+++ javascript/3array.js
@@ -10,3 +10,14 @@ let myarr2 = ["superman", "aquaman" ]
 let merge_array = ["aaaaa","aaass",["aaaaa",["asddsds","saddfsd"]]]
 let flatarray = merge_array.flat(Infinity)
 console.log(flatarray);
+
+console.log(Array.isArray("arba"))
+console.log(Array.isArray(flatarray))//tell is it is array
+console.log(Array.from("arnaz"))////it change written value into array
+console.log(Array.of("arnaz"))//it create a new array
+let sc1 = 100
+let sc2 = 200
+let sc3 = 300
+
+console.log(Array.of(sc1,sc2,sc3))//it create a new array
+// console.log(Array.from(myarr1,myarr2))//it did not change allready cheated arrays 
