c959dc62c829544baa22a0f6a337e950634dce9c
Maokhan <ftal6779@gmail.com>
2024-05-14 07:43:16 +0000

    object2

--- javascript/4object.js
+++ javascript/4object.js
@@ -10,7 +10,19 @@ let first = {
     gender: "male", 
     [hai]:"hai"
 }
-console.log(first.name);
-console.log(first["name"]);
-console.log(typeof first[hai]);
-console.log(first);
+// console.log(first.name);
+// console.log(first["name"]);
+// console.log(typeof first[hai]);
+// console.log(first);
+
+// join two objrct 
+let obj1 = {1:"A",2:"B"}
+let obj2 = {3:"#",4:"s"}
+let obj3 = Object.assign({},obj1,obj2)
+// let obj3 ={...obj1,...obj2}
+console.log(obj3)
+console.log(obj3.hasOwnProperty("1"))
+console.log(Object.keys(obj3))
+console.log(Object.values(obj3))
+console.log(Object.entries(obj3))
+console.log()
