e30e5781e48666d020762c06053995408e864860
Maokhan <ftal6779@gmail.com>
2024-05-14 11:48:23 +0000

    function

---
+++ javascript/5function.js
@@ -0,0 +1,9 @@
+let fun = function(number1,number2){
+    // console.log("hai")   
+    console.log(number1+number2);
+}
+fun //it is refference 
+fun()//it is execution
+fun()/* let fun = function(number1,number2){
+    console.log(number1+number2);
+} when this function execte it output is NaN  */
